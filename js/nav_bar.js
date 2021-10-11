36577b41631ac3cc88577ecace634a7bc2ab35e3
Jack Ploetz <jwploetz@gmail.com>
2021-10-10 23:29:59 -0400

    add nav bar to files

--- js/nav_bar.js
+++ js/nav_bar.js
@@ -7,31 +7,31 @@ var menuStr =
 <li class="dropdown">
   <a href="javascript:void(0)" class="dropbtn">Lessons</a>
   <div class="dropdown-content">
-    <a href="`+directoryDepth+`/lessons/minimal_page.html.html">Minimal Page</a>
-    <a href="`+directoryDepth+`/lessons/gdw.html.html">Getting Down With HTML</a>
-    <a href="`+directoryDepth+`/lessons/block.html.html">Block Level Elements</a>
-    <a href="`+directoryDepth+`/lessons/inline.html.html">Inline Elements</a>
-    <a href="`+directoryDepth+`/lessons/objects.html.html">Object Elements</a>
-    <a href="`+directoryDepth+`/lessons/special.html.html">Special Characters</a>
-    <a href="`+directoryDepth+`/lessons/unordered.html.html">Unordered Lists</a>
-    <a href="`+directoryDepth+`/lessons/ordered.html.html">Ordered Lists</a>
-    <a href="`+directoryDepth+`/lessons/description.html.html">Description Lists</a>
-    <a href="`+directoryDepth+`/lessons/learning_webdev.html.html">Learning Web Dev</a>
-    <a href="`+directoryDepth+`/lessons/decbinocthex.html.html">Number Types Table</a>
-    <a href="`+directoryDepth+`/lessons/mytable.html.html">Courses Table</a>
-    <a href="`+directoryDepth+`/lessons/favorite.html.html">Favorite Websites</a>
-    <a href="`+directoryDepth+`/lessons/gallery1.html.html">Gallery I</a>
-    <a href="`+directoryDepth+`/lessons/gallery2.html.html">Gallery II</a>
+    <a href="`+directoryDepth+`/lessons/minimal_page.html">Minimal Page</a>
+    <a href="`+directoryDepth+`/lessons/gdw.html">Getting Down With HTML</a>
+    <a href="`+directoryDepth+`/lessons/block.html">Block Level Elements</a>
+    <a href="`+directoryDepth+`/lessons/inline.html">Inline Elements</a>
+    <a href="`+directoryDepth+`/lessons/objects.html">Object Elements</a>
+    <a href="`+directoryDepth+`/lessons/special.html">Special Characters</a>
+    <a href="`+directoryDepth+`/lessons/unordered.html">Unordered Lists</a>
+    <a href="`+directoryDepth+`/lessons/ordered.html">Ordered Lists</a>
+    <a href="`+directoryDepth+`/lessons/description.html">Description Lists</a>
+    <a href="`+directoryDepth+`/lessons/learning_webdev.html">Learning Web Dev</a>
+    <a href="`+directoryDepth+`/lessons/decbinocthex.html">Number Types Table</a>
+    <a href="`+directoryDepth+`/lessons/mytable.html">Courses Table</a>
+    <a href="`+directoryDepth+`/lessons/favorite.html">Favorite Websites</a>
+    <a href="`+directoryDepth+`/lessons/gallery1.html">Gallery I</a>
+    <a href="`+directoryDepth+`/lessons/gallery2.html">Gallery II</a>
   </div>
-</li>`;
-/*<li class="dropdown">
-  <a href="javascript:void(0)" class="dropbtn">Important Links</a>
+</li>
+<li class="dropdown">
+  <a href="javascript:void(0)" class="dropbtn">Other Links</a>
   <div class="dropdown-content">
     <a href="https://ict.gctaa.net/sections/webdesign/students.html">Other Websites</a>
+    <a href="http://www.openbookproject.net/tutorials/getdown/index.html">Getting Down With Series</a>
+    <a href="https://en.wikipedia.org/wiki/special:random" target="_blank">Random Wikipedia Page</a>
   </div>
-</li>*/
-console.log(menuStr);
+</li>`;
 
 var menuList = document.getElementById("menuBar");
-console.log(menuList);
 menuList.innerHTML = menuStr;
