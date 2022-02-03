e0b926994ec797de32f288d516752c0ceb24653e
Jack Ploetz <jwploetz@gmail.com>
2022-02-03 14:01:35 -0500

    Fix nav bar

--- js/nav_bar.js
+++ js/nav_bar.js
@@ -1,42 +1,42 @@
 //Thanks Alex for the navbar help
 
-directoryDepth = "..".repeat(window.location.href.replace(/[^/A]/g, "").length-2)
+// directoryDepth = "..".repeat(window.location.href.replace(/[^/A]/g, "").length-2)
 
 var menuStr =
-`<li class="dropdownButton"><a href="`+directoryDepth+`/index.html">Home</a></li>
+`<li class="dropdownButton"><a href="/index.html">Home</a></li>
 <li class="dropdown">
   <a href="javascript:void(0)" class="dropbtn">Lessons 1-4</a>
   <div class="dropdown-content">
-    <a href="`+directoryDepth+`/lessons/minimal_page.html">Minimal Page</a>
-    <a href="`+directoryDepth+`/lessons/gdw.html">Getting Down With HTML</a>
-    <a href="`+directoryDepth+`/lessons/block.html">Block Level Elements</a>
-    <a href="`+directoryDepth+`/lessons/inline.html">Inline Elements</a>
-    <a href="`+directoryDepth+`/lessons/objects.html">Object Elements</a>
-    <a href="`+directoryDepth+`/lessons/special.html">Special Characters</a>
-    <a href="`+directoryDepth+`/lessons/unordered.html">Unordered Lists</a>
-    <a href="`+directoryDepth+`/lessons/ordered.html">Ordered Lists</a>
-    <a href="`+directoryDepth+`/lessons/description.html">Description Lists</a>
-    <a href="`+directoryDepth+`/lessons/learning_webdev.html">Learning Web Dev</a>
+    <a href="/lessons/minimal_page.html">Minimal Page</a>
+    <a href="/lessons/gdw.html">Getting Down With HTML</a>
+    <a href="/lessons/block.html">Block Level Elements</a>
+    <a href="/lessons/inline.html">Inline Elements</a>
+    <a href="/lessons/objects.html">Object Elements</a>
+    <a href="/lessons/special.html">Special Characters</a>
+    <a href="/lessons/unordered.html">Unordered Lists</a>
+    <a href="/lessons/ordered.html">Ordered Lists</a>
+    <a href="/lessons/description.html">Description Lists</a>
+    <a href="/lessons/learning_webdev.html">Learning Web Dev</a>
   </div
 </li>
 <li class="dropdown">
   <a href="javascript:void(0)" class="dropbtn">Lessons 5-8</a>
   <div class="dropdown-content">
-    <a href="`+directoryDepth+`/lessons/decbinocthex.html">Number Types Table</a>
-    <a href="`+directoryDepth+`/lessons/mytable.html">Courses Table</a>
-    <a href="`+directoryDepth+`/lessons/favorite.html">Favorite Websites</a>
-    <a href="`+directoryDepth+`/lessons/gallery1.html">Gallery I</a>
-    <a href="`+directoryDepth+`/lessons/gallery2.html">Gallery II</a>
-    <a href="`+directoryDepth+`/lessons/forms1.html">Form 1</a>
-    <a href="`+directoryDepth+`/lessons/forms2.html">Form 2</a>
-    <a href="`+directoryDepth+`/lessons/forms3.html">Form 3</a>
-    <a href="`+directoryDepth+`/lessons/forms4.html">Form 4</a>
+    <a href="/lessons/decbinocthex.html">Number Types Table</a>
+    <a href="/lessons/mytable.html">Courses Table</a>
+    <a href="/lessons/favorite.html">Favorite Websites</a>
+    <a href="/lessons/gallery1.html">Gallery I</a>
+    <a href="/lessons/gallery2.html">Gallery II</a>
+    <a href="/lessons/forms1.html">Form 1</a>
+    <a href="/lessons/forms2.html">Form 2</a>
+    <a href="/lessons/forms3.html">Form 3</a>
+    <a href="/lessons/forms4.html">Form 4</a>
   </div>
 </li>
 <li class="dropdown">
   <a href="javascript:void(0)" class="dropbtn">Experiments</a>
   <div class="dropdown-content">
-    <a href="`+ directoryDepth + `/experiments/navidad/index.html">Navidad</a>
+    <a href="/experiments/navidad/index.html">Navidad</a>
   </div>
 </li>
 <li class="dropdown">
